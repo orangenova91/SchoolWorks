b8c3dabad8b13906c071e3be84fd6859a9d264a4
Ruel9 <univcos91@gmail.com>
2026-01-12 14:49:59 +0900

    version 최신화

--- lib/i18n.ts
+++ lib/i18n.ts
@@ -6,7 +6,7 @@ export const defaultLocale: Locale = "ko";
 export const translations = {
   ko: {
     app: {
-      version: "Ver.27",
+      version: "Ver.29",
     },
     auth: {
       login: "로그인",
@@ -136,7 +136,7 @@ export const translations = {
   },
   en: {
     app: {
-      version: "Ver.27",
+      version: "Ver.29",
     },
     auth: {
       login: "Login",
